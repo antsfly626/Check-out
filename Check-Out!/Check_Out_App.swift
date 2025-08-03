4ce3216a48b961cb3532e78a4d1bea218ec662e9
Tanya Hingorani <tanyahingorani526@gmail.com>
2025-08-03 00:11:04 -0700

    Add files via upload
    initial swift ui commit

---
+++ Check-Out!/Check_Out_App.swift
@@ -0,0 +1,17 @@
+//
+//  Check_Out_App.swift
+//  Check-Out!
+//
+//  Created by ANTS on 8/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Check_Out_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
